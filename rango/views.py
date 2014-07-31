2b40d10316559bdc98e887c9dd5abc19cedadec5
wangc <chaoswong@qq.com>
2014-07-31 17:37:18 +0800

    templates and statics OK

--- rango/views.py
+++ rango/views.py
@@ -9,4 +9,6 @@ def index(request):
     return render_to_response('rango/index.html', context_dict, context)
 
 def about(request):
-    return HttpResponse('Rango Says: Here is the about page.')
+    context = RequestContext(request)
+    context_dict = {'message': 'Here is the about page'}
+    return render_to_response('rango/about.html', context_dict, context)
