521f179604dc9b35ba6ea6d0f61503c3128751f3
wangc <chaoswong@qq.com>
2014-08-05 14:58:34 +0800

    finish 6

---
+++ rango/utils.py
@@ -0,0 +1,5 @@
+def encode(name):
+    return name.replace(' ','_')
+
+def decode(url):
+    return url.replace('_',' ')
--- rango/views.py
+++ rango/views.py
@@ -1,14 +1,44 @@
-
 from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 
+from rango.models import Category
+from rango.models import Page
+
+from utils import encode,decode
+
+
 def index(request):
     context = RequestContext(request)
-    context_dict = {'boldmessage': 'I am bold font from the context'}
+
+    category_list = Category.objects.order_by('-likes')[:5]
+    pages_list = Page.objects.order_by('-views')[:5]
+    context_dict = {'categories':category_list,
+                    'pages':pages_list}
+
+    for category in category_list:
+        category.url = encode(category.name)
+
     return render_to_response('rango/index.html', context_dict, context)
 
 def about(request):
     context = RequestContext(request)
     context_dict = {'message': 'Here is the about page'}
     return render_to_response('rango/about.html', context_dict, context)
+
+def category(request, category_name_url):
+    context = RequestContext(request)
+
+    category_name = decode(category_name_url)
+    context_dict = {'category_name':category_name}
+
+    try:
+        category = Category.objects.get(name=category_name)
+        pages = Page.objects.filter(category=category)
+
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        pass
+
+    return render_to_response('rango/category.html', context_dict, context)
