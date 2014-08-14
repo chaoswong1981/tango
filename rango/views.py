eb7c94cd82f746f67c99183ca19424f667f08dd7
wangc <chaoswong@qq.com>
2014-08-14 08:22:04 +0800

    ok,test

--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,7 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext
 from django.shortcuts import render_to_response
+from django.contrib.auth import authenticate,login
 
 from utils import encode_url,decode_url
 
@@ -9,6 +10,7 @@ from rango.models import Page
 
 from rango.forms import CategoryForm
 from rango.forms import PageForm
+from rango.forms import UserForm, UserProfileForm
 
 
 def index(request):
@@ -87,3 +89,59 @@ def add_page(request, category_name_url):
     else:
         page = PageForm()
     return render_to_response('rango/add_page.html',{'category_name_url':category_name_url,'page':page},context)
+
+def register(request):
+    context = RequestContext(request)
+
+    registered = False
+
+    if request.method == 'POST':
+        user_form = UserForm(data=request.POST)
+        profile_form = UserProfileForm(data=request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+
+            print user.password
+            user.set_password(user.password)
+            user.save()
+
+            profile = profile_form.save(commit=False)
+            profile.user = user
+
+            if 'picture' in request.FILES:
+                profile.picture = request.FILES['pictures']
+            profile.save()
+
+            registered = True
+        else:
+            print user_form.errors, profile_form.errors
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+
+    return render_to_response(
+        'rango/register.html',
+        {'user_form':user_form, 'profile_form':profile_form, 'registered':registered},
+        context)
+
+def user_login(request):
+    context = RequestContext(request)
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect('/rango/')
+            else:
+                return HttpResponse('Your Rango account is disabled.')
+        else:
+            print 'Invalid login detail: {0}, {1}'.format(username,password)
+            return HttpResponse('Invalid login details supplied.')
+    else:
+        return render_to_response('rango/login.html', {}, context)
