9a374e14d3f694e0d4fb13d97edc767e65e6000f
wangc <chaoswong@qq.com>
2014-08-15 15:11:31 +0800

    finish 8

--- rango/views.py
+++ rango/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext
 from django.shortcuts import render_to_response
-from django.contrib.auth import authenticate,login
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
 
 from utils import encode_url,decode_url
 
@@ -141,7 +142,15 @@ def user_login(request):
             else:
                 return HttpResponse('Your Rango account is disabled.')
         else:
-            print 'Invalid login detail: {0}, {1}'.format(username,password)
-            return HttpResponse('Invalid login details supplied.')
+            return render_to_response('rango/login.html', {'error':'Invalid username or password.'}, context)
     else:
         return render_to_response('rango/login.html', {}, context)
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect('/rango/')
+
+@login_required
+def restricted(request):
+    return HttpResponse("Since you're logged in, you can see this text!")
